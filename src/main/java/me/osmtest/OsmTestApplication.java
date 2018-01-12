ef3a131ee786843396c17197f7a31b071226797e
Pavel Taruts <taruts@improveitgroup.com>
2018-01-12 16:13:19 +0700

    -

---
+++ src/main/java/me/osmtest/OsmTestApplication.java
@@ -0,0 +1,12 @@
+package me.osmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsmTestApplication.class, args);
+	}
+}
